c5a1d9290d6cd84844342ef56d59cc2fbf23b6cf
Berisdaa <68280730+Berisdaa@users.noreply.github.com>
2020-10-20 11:57:22 +0700

    Tugas Minggu Ke-15
    Tugas PHP Minggu Ke-15
    
    Video 46-50

---
+++ Tugas Minggu Ke-15/Project Restoran/Menu/insert.php
@@ -0,0 +1,67 @@
+<?php 
+    $row=$db->getALL("SELECT * FROM tblkategori ORDER BY kategori ASC");
+?>
+
+<h3>Insert Menu</h3>
+
+<div class="form-group">
+<form action="" method="post" enctype="multipart/form-data">
+
+    <div class="form-group w-50"> <!--  -->
+
+        <label for="">Kategori</label> <br>
+        <select name="idkategori" id="">
+        <?php foreach($row as $r): ?>
+        <option value="<?php echo $r['idkategori'] ?>"> <?php echo $r['kategori'] ?>  </option>
+        <?php endforeach ?>   
+        </select>
+        
+    </div>
+    
+    <div class="form-group w-50"> <!-- Insert Nama Menu -->
+        <label for="">Nama menu</label> 
+        <input type="text" name="menu" id="" required placeholder="Isi menu" class="form-control">
+    </div>
+
+    <div class="form-group w-50"> <!-- Insert Harga Menu -->
+        <label for="">Harga</label>
+        <input type="text" name="harga" id="" number required placeholder="Isi harga" class="form-control">
+    </div>
+
+    <div class="form-group w-50"> <!-- Insert Gambar Menu -->
+        <label for="">Gambar</label> <br>
+        <input type="file" name="gambar">
+    </div>
+
+
+    <div>
+        <input type="submit" name="simpan" value="Simpan" class="btn btn-primary">
+    </div>
+    </form>
+
+</div>
+
+<?php 
+
+    if (isset($_POST['simpan'])) {
+        $idkategori = $_POST['idkategori'];
+        $menu = $_POST['menu'];
+        $harga = $_POST['harga'];
+        $gambar = $_FILES['gambar']['name'];
+        $temp = $_FILES['gambar']['tmp_name'];
+
+        if (empty($gambar)){
+            echo "<h3> GAMBAR TIDAK DI INSERT </h3>";
+        }else {
+            $sql = "INSERT INTO tblmenu VALUES ('',$idkategori,'$menu','$gambar',$harga)";
+            move_uploaded_file($temp,'../Gambar/'.$gambar);
+
+            $db->runSQL($sql);
+            header("location:?f=menu&m=select");
+        }
+
+        
+    }
+
+?>
+
